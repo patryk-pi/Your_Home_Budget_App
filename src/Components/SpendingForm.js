f6fa8de5db879b9834f071779bf3ee433b8c90e8
patryk-pi <patryk.piasecki1@gmail.com>
2023-03-02 17:56:05 +0100

    fixed format of date in SpendingsTable and SpendingsForm

--- src/Components/SpendingForm.js
+++ src/Components/SpendingForm.js
@@ -13,7 +13,7 @@ const SpendingForm = ({add}) => {
     const [categories, setCategories] = useState([]);
     const [category, setCategory] = useState('');
     const [description, setDescription] = useState('');
-    const [amount, setAmount] = useState(0)
+    const [amount, setAmount] = useState('')
     const [date, setDate] = useState(dayjs(new Date()).format('DD/MM/YYYY'));
     const [month, setMonth] = useState(dayjs(new Date()).format('MM/YYYY'));
 
@@ -31,12 +31,20 @@ const SpendingForm = ({add}) => {
 
     const handleSubmit = e => {
         e.preventDefault();
+
+        if (!category || !amount || !date) return
         add({
             category,
             description,
             amount,
-            date
-        })
+            date: dayjs(new Date(date)).format('DD/MM/YYYY')
+        });
+
+        setAmount('');
+        setCategory('');
+        setDate(dayjs(new Date()).format('MM/DD/YYYY'));
+        setDescription('')
+
     }
 
     useEffect(() => {
@@ -51,6 +59,10 @@ const SpendingForm = ({add}) => {
         setFilteredCategory(categories.filter(({type}) => type === "expense"));
     }, [categories])
 
+    useEffect(() => {
+
+    })
+
 
     return (
         <>
@@ -61,7 +73,7 @@ const SpendingForm = ({add}) => {
                 width: "40%",
                 border: '1px solid lightgray',
                 overflow: 'scroll',
-                maxHeight: '40rem',
+                maxHeight: '53rem',
                 borderRadius: '20px',
                 padding: '1rem'
 
@@ -76,31 +88,38 @@ const SpendingForm = ({add}) => {
                         padding: '5rem',
 
                     }}>
-                        <InputLabel label={"Kategoria"}>Kategoria</InputLabel>
 
-                            <Select
+                            <TextField
                                 value={category}
                                 label="Kategoria"
                                 onChange={e => setCategory(e.target.value)}
+                                select={true}
                             >
                                 {filteredCategory.map(({description, id}) => {
                                     return (
-                                        <MenuItem key={id} value={description}>{description}</MenuItem>
+                                        <MenuItem sx={{
+                                            marginBottom: '.8rem',
+                                            fontSize: '1.2rem'
+                                        }} key={id} value={description}>{description}</MenuItem>
                                     )
                                 })}
-                            </Select>
+                            </TextField>
                         <TextField label={"Opis"} type={"text"}
-                                   onChange={e => setDescription(e.target.value)}></TextField>
-                        <TextField label={"Kwota"} type={'text'} InputProps={{inputProps: {min: 0}}} onChange={(e) => {
+                                   onChange={e => setDescription(e.target.value)} value={description}></TextField>
+                        <TextField value={amount} label={"Kwota"} type={'text'} InputProps={{inputProps: {min: 0}}} onChange={(e) => {
                             const value = e.target.value;
                             // validate input using regex to accept decimal numbers with up to 2 decimal places
                             setAmount(parseFloat(value))
                         }}>
                         </TextField>
                         <LocalizationProvider dateAdapter={AdapterDayjs}>
-                            <DatePicker inputFormat={'DD/MM/YYYY'} onChange={(date) => setDate(dayjs(date).format())}
+                            <DatePicker inputFormat={'DD/MM/YYYY'} onChange={(date) => {
+                                console.log(dayjs(date).format('DD/MM/YYYY'))
+                                setDate(dayjs(date).format('MM/DD/YYYY'))
+                            }
+                            }
                                         value={date}
-                                        renderInput={(props) => <TextField {...props}/>} label={'Select date'}/>
+                                        renderInput={(props) => <TextField {...props}/>} label={'Select date'} />
                         </LocalizationProvider>
                         <Button type={'submit'}>Dodaj</Button>
                     </FormControl>
