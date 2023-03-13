dd6b789bba3d96a702740d6be4b423fe880b297c
patryk-pi <patryk.piasecki1@gmail.com>
2023-03-13 21:17:42 +0100

    added colors to graphs

--- src/Components/expenseStructure/ExpenseAndIncomeGraph.js
+++ src/Components/expenseStructure/ExpenseAndIncomeGraph.js
@@ -3,15 +3,16 @@ import React from "react";
 
 import SpendingsOverviewHeader from "../SpendingsOverviewHeader";
 import PieChart from "./PieChart";
+import MonthlyBalanceOverview from "../MonthlyBalanceOverview";
 
 const ExpenseAndIncomeGraph = () => {
-
-
 
     return (
         <>
             <SpendingsOverviewHeader />
-            <PieChart />
+            <MonthlyBalanceOverview />
+            <PieChart transaction={'income'}/>
+            <PieChart transaction={'expense'}/>
         </>
     )
 }
--- src/Components/expenseStructure/PieChart.js
+++ src/Components/expenseStructure/PieChart.js
@@ -2,38 +2,68 @@ import React, {useContext} from 'react';
 import {Pie} from 'react-chartjs-2';
 import {AppContext} from '../../context/AppProvider';
 
-import {Chart, ArcElement} from 'chart.js'
+import {Chart, ArcElement, Colors, Tooltip} from 'chart.js'
 
-Chart.register(ArcElement)
+Chart.register(ArcElement, Colors, Tooltip)
 
-const PieChart = () => {
+const PieChart = ({transaction}) => {
     const {operations, filterOperationsByMonth} = useContext(AppContext);
 
-    console.log([...new Set(operations
-        .map(({category}) => category))
-    ],)
+
 
 
 
     // Format data for Pie chart
+    const categories = [...new Set(operations.map(({category}) => category))];
+
+
     const data = {
-        labels: [...new Set(operations
-            .map(({category}) => category))
-        ],
+        labels: categories,
         datasets: [
             {
                 label: 'Zł',
-                data: operations
-                    .filter((operation) => filterOperationsByMonth(operation))
-                    .filter(({amount}) => amount < 0)
-                    .map(({amount}) => amount),
+                data: categories.map(category =>
+                    operations
+                        .filter(operation => filterOperationsByMonth(operation))
+                        .filter(operation => operation.category === category)
+                        .filter(({amount}) => transaction === 'income' ? amount > 0 : amount < 0)
+                        .reduce((sum, {amount}) => sum + amount, 0)
+                ),
+              /*  backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#00FF00'],
+                hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#00FF00'],*/
             },
         ],
     };
 
+    const tooltipLabelCallback = (tooltipItem) => {
+        const label = data.labels[tooltipItem.index];
+        const value = data.datasets[0].data.find((obj) => obj.label === label)?.value ?? 0;
+        return `${label}: ${value} Zł`;
+    };
 
 
-    return <Pie data={data}/>;
+    const options = {
+        plugins: {
+            tooltip: {
+                enabled: true,
+                mode: 'index',
+                callbacks: {
+                    label: tooltipLabelCallback,
+                },
+            },
+        },
+    };
+
+
+    if (data.datasets[0].data.every(sum => sum === 0)) {
+        return (
+            <>
+                <h1>Brak danych dotyczących {transaction === 'income' ? 'wpływów' : 'wydatków'} :(</h1>
+            </>
+        )
+    }
+
+    return <Pie data={data} options={options}/>;
 };
 
 export default PieChart;
