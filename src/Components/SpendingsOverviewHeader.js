076e3389cc16cf9d3b248bed71049cdf0b17e941
patryk-pi <patryk.piasecki1@gmail.com>
2023-09-07 21:32:02 +0200

    removed unnecessary comment

--- src/Components/SpendingsOverviewHeader.js
+++ src/Components/SpendingsOverviewHeader.js
@@ -1,46 +1,50 @@
-import React, {useContext} from "react";
-import {Box, IconButton} from "@mui/material";
+import React, { useContext } from "react";
+import { Box, IconButton } from "@mui/material";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import {AppContext} from "../context/AppProvider";
-
-
+import { AppContext } from "../context/AppProvider";
 
 const SpendingsOverviewHeader = () => {
-  /*  const styles = useStyles();*/
-    const {
-        currentYear,
-        currentMonthString,
-        nextMonth,
-        prevMonth,
-
-    } = useContext(AppContext)
+    const { currentYear, currentMonthString, nextMonth, prevMonth } =
+        useContext(AppContext);
 
     return (
-        <Box sx={{
-            display: 'flex',
-            justifyContent: "space-between",
-            alignItems: 'center',
-            padding: '3rem 0 0',
-            marginBottom: '2rem'
-        }}>
-            <IconButton size='large' onClick={prevMonth} sx={{
-                marginLeft: '27rem',
-                boxShadow: 3,
-                bgcolor: 'white'
-            }}>
-                <ArrowBackIosNewIcon fontSize={'large'}/>
+        <Box
+            sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                padding: "3rem 0 0",
+                marginBottom: "2rem",
+            }}
+        >
+            <IconButton
+                size="large"
+                onClick={prevMonth}
+                sx={{
+                    marginLeft: "27rem",
+                    boxShadow: 3,
+                    bgcolor: "white",
+                }}
+            >
+                <ArrowBackIosNewIcon fontSize={"large"} />
             </IconButton>
-            <h1 className={'app__container__heading'}>{currentMonthString} {currentYear}</h1>
-            <IconButton size='large' onClick={nextMonth} sx={{
-                marginRight: '27rem',
-                boxShadow: 3,
-                bgcolor: 'white'
-            }}>
-                <ArrowForwardIosIcon fontSize={'large'}/>
+            <h1 className={"app__container__heading"}>
+                {currentMonthString} {currentYear}
+            </h1>
+            <IconButton
+                size="large"
+                onClick={nextMonth}
+                sx={{
+                    marginRight: "27rem",
+                    boxShadow: 3,
+                    bgcolor: "white",
+                }}
+            >
+                <ArrowForwardIosIcon fontSize={"large"} />
             </IconButton>
         </Box>
-    )
-}
+    );
+};
 
-export default SpendingsOverviewHeader
+export default SpendingsOverviewHeader;
